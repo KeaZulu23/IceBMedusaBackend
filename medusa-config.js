a4463c6ce8d668e30d593ea87caeb4fdc6b022c5
KeaZulu23 <79447627+KeaZulu23@users.noreply.github.com>
2023-10-22 17:38:43 +0200

    cors update

--- medusa-config.js
+++ medusa-config.js
@@ -23,7 +23,7 @@ try {
 
 // CORS when consuming Medusa from admin
 const ADMIN_CORS =
-  process.env.ADMIN_CORS || "https://ice-b-medusa-admin.vercel.app";
+  process.env.ADMIN_CORS || "https://heartfelt-rabanadas-e27f4c.netlify.app";
 
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "https://ice-b-medusa-storefront.vercel.app";
